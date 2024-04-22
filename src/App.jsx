e98bab28c789cfa2308b32234d9e0618562650fc
Nuh00 <barkatnoah35@gmail.com>
2024-04-21 21:23:04 -0500

    navbar bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
