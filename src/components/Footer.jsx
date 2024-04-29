12b80dc1433b0c2c90311edf57ac5730cdd0ce64
Nuh00 <barkatnoah35@gmail.com>
2024-04-29 17:29:00 -0500

    Finnaly finished with this landing page, learned a lot about link tags and how the entire process of components work in react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,54 +61,54 @@ function Footer() {
 
       <section className="social-media">
         <div className="social-media-wrap">
-            <div className="footer-logo">
-                <Link to="/" className="social-logo">
-                TRVL <i className="fab fa-typo3" />
-                </Link>
-            </div>
-            <small className="website-rights">TRVL © 2021</small>
-            <div className="social-icons">
-                <Link
-                className="social-icon-link facebook"
-                to="/"
-                target="_blank"
-                aria-label="Facebook"
-                >
-                <i className="fab fa-facebook-f" />
-                </Link>
-                <Link
-                className="social-icon-link instagram"
-                to="/"
-                target="_blank"
-                aria-label="Instagram"
-                >
-                <i className="fab fa-instagram" />
-                </Link>
-                <Link
-                className="social-icon-link youtube"
-                to="/"
-                target="_blank"
-                aria-label="Youtube"
-                >
-                <i className="fab fa-youtube" />
-                </Link>
-                <Link
-                className="social-icon-link twitter"
-                to="/"
-                target="_blank"
-                aria-label="Twitter"
-                >
-                <i className="fab fa-twitter" />
-                </Link>
-                <Link
-                className="social-icon-link linkedin"
-                to="/"
-                target="_blank"
-                aria-label="LinkedIn"
-                >
-                <i className="fab fa-linkedin" />
-                </Link>
-            </div>
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL © 2021</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f" />
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram" />
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube" />
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter" />
+            </Link>
+            <Link
+              className="social-icon-link linkedin"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin" />
+            </Link>
+          </div>
         </div>
       </section>
     </div>
